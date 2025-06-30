6dc8ed8d3e27ee46bd0e5884bc22c3138054b55a
OpAue <kkt23917@naver.com>
2025-06-30 19:46:20 +0900

    set : 프로젝트세팅

---
+++ src/main/java/com/example/llspring/LlSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.llspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LlSpringApplication.class, args);
+    }
+
+}
